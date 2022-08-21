a0d767f4bca46a41e9f0ea7d0c75065db71aeb86
dankiprop <dankiproh@gmail.com>
2022-08-21 12:45:00 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random() * 10);
+
+const num3 = 24
+const num4 = 6
+const mod = 24/6
+
+const max = Math.max(1, 2, 3, 4, 5, 14, 11, 20);
